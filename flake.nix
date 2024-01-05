d640bb1aca6f6dae74869c12302489750a7be44a
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-01-05 23:11:32 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -33,25 +33,6 @@
       packageName = "safety-for-cpp";
     in {
 
-      # devShells.x86_64-linux.default = pkgs.mkShell {
-      #   buildInputs = with pkgs; [ klee ];
-      #   #inputsFrom = builtins.attrValues self.packages.${system};
-      # };
-
-      packages.x86_64-linux.get_sign =
-        # Notice the reference to nixpkgs here.
-        with import nixpkgs { system = "x86_64-linux"; };
-        stdenv.mkDerivation {
-          buildInputs = with pkgs; [ clang ];
-          nativeBuildInputs = with pkgs; [ klee ];
-          name = "get_sign";
-          src = self;
-          dontStrip = true;
-          buildPhase = "clang -emit-llvm -c -g -O0 -Xclang -disable-O0-optnone ./src/get_sign.c";
-          installPhase = "mkdir -p $out/bitcode; install -t $out/bitcode get_sign.bc";
-        };
-
-
       packages.x86_64-linux.coreutils_src =
         # Notice the reference to nixpkgs here.
         with import nixpkgs { system = "x86_64-linux"; };
@@ -77,38 +58,17 @@
 
         };
 
-      # Main package that uses the configured source
-      packages.x86_64-linux.coreutils_extract =
-        with import nixpkgs { system = "x86_64-linux"; };
-        pkgs.stdenv.mkDerivation {
-          nativeBuildInputs = [ self.packages.x86_64-linux.coreutils pkgs.clang pkgs.llvm ];
-          buildInputs = [ pkgs.llvm ];
-          pname = pkgs.coreutils.meta.name;
-          version = pkgs.coreutils.version;
-          src = self.packages.x86_64-linux.coreutils;
-          installPhase = ''
-    ls -l
-    find bin/ -executable -type f | xargs -I '{}' extract-bc -l llvm-link-14 '{}'
-    ls -l
-    mkdir -p $out/bitcode; cp *.bc $out/bitcode/
-  '';
-        };
       packages.x86_64-linux.myapp = mkPoetryApplication {
         projectDir = self;
         overrides = p2n-overrides;
       };
-
-      # devShells.x86_64-linux.default = mkPoetryEnv {
-      #     projectDir = self;
-      #     overrides = p2n-overrides;
-      # };
 
       devShells.x86_64-linux.default = pkgs.mkShell {
         inputsFrom = [ self.packages.x86_64-linux.myapp ];
         packages = [ pkgs.poetry ];
       };
 
-      packages.x86_64-linux.default = self.packages.x86_64-linux.coreutils_extract;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.coreutils;
 
     };
 }
